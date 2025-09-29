f2f5dcc4af883c25dc659f46e16c2121d3bb2900
DESKTOP-485HQ7U\Randy <randy.gatchalian@medisureonline.com>
2025-09-29 09:15:08 +0800

    Mobile view fixes

--- src/components/ServicesOverview.vue
+++ src/components/ServicesOverview.vue
@@ -1,36 +1,50 @@
 <template>
-  <v-container id="services-overview" class="py-10" fluid>
-    <h2 class="text-h4 font-weight-bold mb-8 text-center">
-      Our Services
-    </h2>
+	<section
+		id="services-overview"
+		class="py-12"
+	>
+		<v-container fluid>
+			<v-row>
+				<v-col cols="12">
+					<h2 class="text-h4 font-weight-bold mb-8 text-center">
+						Our Services
+					</h2>
+				</v-col>
 
-    <v-row dense>
-      <v-col
-        v-for="(service, index) in SERVICES_OVERVIEW"
-        :key="index"
-        cols="12"
-        sm="6"
-        md="4"
-      >
-        <v-card class="h-100" link>
-          <v-img
-            :src="service.image"
-            height="200"
-            cover
-          ></v-img>
+				<v-col
+					v-for="(service, index) in SERVICES_OVERVIEW"
+					:key="index"
+					cols="12"
+					md="4"
+				>
+					<v-card
+						elevation="4"
+						class="rounded-lg"
+						link
+					>
+						<v-img
+							:src="service.image"
+							height="200px"
+							cover
+						></v-img>
 
-          <v-card-title>{{ service.title }}</v-card-title>
-          <v-card-text>{{ service.description }}</v-card-text>
+						<v-card-title>{{ service.title }}</v-card-title>
+						<v-card-text>{{ service.description }}</v-card-text>
 
-          <v-card-actions>
-            <v-btn color="primary" variant="text">Learn More</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+						<v-card-actions>
+							<v-btn
+								color="primary"
+								variant="text"
+								>Learn More</v-btn
+							>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
+	</section>
 </template>
 
 <script setup lang="ts">
-import { SERVICES_OVERVIEW } from "../data/services-overview";
+import { SERVICES_OVERVIEW } from '../data/services-overview'
 </script>
